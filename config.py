7612f8979c590247fb471b2e80f09b55b3b0b1af
chatraleizaz <39851395+chatraleizaz@users.noreply.github.com>
2025-04-20 01:58:24 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import datetime
-from os import environ 
+from os import environ
 
-#Dont Remove My Credit @Silicon_Bot_Update 
-#This Repo Is By @Silicon_Official 
+# Dont Remove My Credit @Silicon_Bot_Update 
+# This Repo Is By @Silicon_Official 
 # For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
 
 class Config:
@@ -10,17 +10,22 @@ class Config:
     API_HASH = environ.get("API_HASH", "414f022bf4a6620cb4bba3505605e374")
     BOT_TOKEN = environ.get("BOT_TOKEN", "7931443175:AAEXfOF4QrmQrhPuGFSUh3Pr6lqiSOO4VU0") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://jauharobaid:1VAOB4Fm5gvM2YKS@cluster0.1nc4nru.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
+    
+    # MongoDB connection and database name should be properly set
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://jauharobaid:1VAOB4Fm5gvM2YKS@cluster0.1nc4nru.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    
+    # Ensure a valid database name is set
+    DATABASE_NAME = environ.get("DATABASE_NAME", "default_database_name")  # Add a fallback default name here
+    
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7098324238').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002684943176'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1002684943176") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")
     PORT = environ.get('PORT', '8080')
-    
-#Dont Remove My Credit @Silicon_Bot_Update 
-#This Repo Is By @Silicon_Official 
-# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
+
+# Dont Remove My Credit @Silicon_Bot_Update 
+# This Repo Is By @Silicon_Official 
+# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz
 
    
 class temp(object): 
@@ -30,6 +35,6 @@ class temp(object):
     BANNED_USERS = []
     IS_FRWD_CHAT = []
     
-#Dont Remove My Credit @Silicon_Bot_Update 
-#This Repo Is By @Silicon_Official 
-# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
+# Dont Remove My Credit @Silicon_Bot_Update 
+# This Repo Is By @Silicon_Official 
+# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz
