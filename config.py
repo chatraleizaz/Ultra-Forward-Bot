513f8bd685b6711928b5c03be89ecc95c3e7ac10
Devolve <39851395+chatraleizaz@users.noreply.github.com>
2025-07-18 17:20:35 +0500

    Update config.py

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ from os import environ
 
 class Config:
     API_ID = environ.get("API_ID", "27876808")
-    API_HASH = environ.get("API_HASH", "414f022bf4a6620cb4bba3505605e374")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "8028951353:AAHFvbTlmIWzunmf3-P9Ng5K1EOe2EsWbSQ") 
+    API_HASH = environ.get("API_HASH", "414f02a6620cb4bba3505605e374")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8051353:FvbTlmIWzunmf3-P9Ng5K1EOe2EsWbSQ") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://jauharobaid:1VAOB4Fm5gvM2YKS@cluster0.1nc4nru.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "jauharobaid")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7098324238').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002567434243'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/+cBGPzTMPUiA2MGY0") 
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://jauharoB4Fm5gvM2YKS@cluster0.1nc4nru.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "jauobaid")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7324238').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002434243'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.mezTMPUiA2MGY0") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")
     PORT = environ.get('PORT', '8080')
     
